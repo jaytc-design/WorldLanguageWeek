252b3df1bedc3aaa412f4255d7513784938e1afb
John <johnthomascanton@gmail.com>
2024-03-19 22:07:23 -0400

    Co-authored-by: Wilbur Wei <wilburcoding@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,61 @@
+const QUOTES = [
+  {
+    "src":"./assets/quoteImages/dilanAlex.JPG",
+    "name":"Dilan Love",
+    "caption":"They found true lov",
+    "alt":"gay couple"
+  },
+{
+    "src":"./assets/quoteImages/india.png",
+    "name":"Hinduism",
+    "caption":"He went to india",
+    "alt":"archondoraydon"
+}
+
+];
+
+window.onload = function() {
+    let cmain = document.getElementById("cmain");
+    let cindicator = document.getElementById("cindicator");
+
+    for (let i = 0; i < QUOTES.length; i++) {
+        let quote = QUOTES[i];
+
+        let carouselItem = document.createElement("div");
+        carouselItem.classList.add("carousel-item");
+        if (i == 0) {
+            carouselItem.classList.add("active");
+        }
+
+        let img = document.createElement("img");
+        img.src = quote.src;
+        img.classList.add("person-image");
+        img.classList.add("d-block");
+        img.classList.add("w-100");
+        img.title = quote.alt;
+        carouselItem.appendChild(img);
+
+        let carouselCaption = document.createElement("div");
+        carouselCaption.classList.add("carousel-caption");
+        let h5 = document.createElement("h5");
+        h5.innerHTML = quote.name;
+        let p = document.createElement("p");
+        p.innerHTML = quote.caption;
+        carouselCaption.appendChild(h5);
+        carouselCaption.appendChild(p);
+        carouselItem.appendChild(carouselCaption);
+
+        cmain.appendChild(carouselItem);
+
+        let button = document.createElement("button");
+        button.type = "button";
+        button.setAttribute("data-bs-target", "#quoteCarousel");
+        button.setAttribute("data-bs-slide-to", i);
+        if (i == 0) {
+            button.classList.add("active");
+            button.setAttribute("aria-current", "true");
+        }
+        button.setAttribute("aria-label", "Slide " + (i + 1));
+        cindicator.appendChild(button); 
+    }
+}
